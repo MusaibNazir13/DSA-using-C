55363caed0168a89fb2aa4bbb0c25b3e200c5d62
GeAr <ytgearsnation@gmail.com>
2025-12-19 20:59:09 +0530

    added the menu for the Singly Circular Linked list

--- singly_circular_list.c
+++ singly_circular_list.c
@@ -63,6 +63,7 @@ struct Node * insertAtPos(int pos,int data){
         if (temp->next==tail->next)
         {
             free(nNode);
+            printf("\ninvalid position , hence appending at last");
             tail = insertEnd(data); //if the position is not valid appending it to last pos instead
             return tail;
         }
@@ -87,6 +88,7 @@ void deleteBeg(){
     }
     struct Node * temp = tail->next;
     tail->next = tail->next->next;
+    printf("\n%d deleted at beginning.",temp->data);
     free(temp);
 }
 void deleteEnd(){
@@ -106,8 +108,35 @@ void deleteEnd(){
     }
     
     temp->next=tail->next;
+    printf("\n%d deleted at end",tail->data);
     free(tail);
     tail = temp;
+}
+void deletePos(int pos){
+    if (tail==NULL)
+    {
+        printf("already empty");
+    }
+    if(tail->next==tail){
+        free(tail);
+        tail=NULL;
+        return;
+    }
+    struct Node * temp = tail->next;
+    for(int i=0; i < pos-1; i++){
+        temp=temp->next;
+        if (temp->next==tail)
+        {
+            printf("\nthe position is invalid.");
+            return;
+        }
+        
+    }
+    struct Node * temp2 = temp->next;
+    temp->next = temp->next->next;
+    printf("\n%d deleted at %d",temp->data,pos);
+    free(temp);
+    free(temp2);
 }
 void display(){
     if (tail==NULL)
@@ -123,21 +152,63 @@ void display(){
     }while(temp!=tail->next);
 
 }
+
+void menu(){
+    printf("\n1 : for Insert At beginning\n2: for insert at end\n3: for insert at any pos\n4: for delete at beginning\n5: for delete at pos\n6: for delete at end\n7: for displaying the list\n8: for HELP MENU\n9: for Exit");
+}
 int main(){
-        tail=insertBeg(45);
-        insertBeg(10);
-        insertBeg(60);
-        tail = insertEnd(41); 
-        tail = insertAtPos(3,20);
-        deleteBeg();
-        deleteBeg();
-        // deleteBeg();
-        // deleteBeg();
-        // deleteBeg();
-        // deleteBeg();
-        deleteEnd();
-        deleteEnd();
-        deleteEnd();
-        display();
+        int choice;
+        int data;
+        int pos;
+        menu();
+       while(1){
+        
+        printf("\nEnter your choice: ");
+        scanf("%d",&choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter the data you want to insert at beginning (Integer value): ");
+            scanf("%d",&data);
+            tail=insertBeg(data);
+            break;
+        case 2:
+            printf("\nEnter the data you want to insert at ending (Integer value): ");
+            scanf("%d",&data);
+            tail=insertEnd(data);
+            break;
+        case 3:
+            printf("\nEnter the data & pos separated by space respectively (Integer value): ");
+            scanf("%d %d",&data,&pos);
+            tail=insertAtPos(pos,data);
+            break;
+        case 4:
+            deleteBeg();   
+            break;
+        case 5:
+            printf("\nEnter the position you want to delete: ");
+            scanf("%d",&pos);
+            deletePos(pos);
+            break;
+        case 6:
+            deleteEnd();
+            break;
+        case 7:
+            printf("Here is the list: ");
+            display();
+            break; 
+        case 8:
+            menu();    
+            break;
+        case 9:
+            printf("Good Bye...........");
+            exit(1);               
+        default:
+            printf("\nThe choice entered is invalid");
+            break;
+        }
+    }
 
 }
+
