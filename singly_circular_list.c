9b85418f1b5fe840d78d8e127fb74b957388a00e
GeAr <ytgearsnation@gmail.com>
2025-12-19 19:07:36 +0530

    added delete at end and modified the delete beg fn with handling of case where there is only one node in the list

--- singly_circular_list.c
+++ singly_circular_list.c
@@ -52,23 +52,62 @@ struct Node * insertAtPos(int pos,int data){
     
     if (pos==1)
     {
+        free(nNode);
         return insertBeg(data);
     }
     
-
-    while (pos!=2)
+    
+    for(int i = 1; i < pos-1;i++)
     {
         temp = temp->next;
-        if (temp==tail->next)
+        if (temp->next==tail->next)
         {
-            return insertEnd(data);
+            free(nNode);
+            tail = insertEnd(data); //if the position is not valid appending it to last pos instead
+            return tail;
         }
-         pos--;
+         printf("\n%d\n",i);
     }
+   
     nNode->next=temp->next;
     temp->next = nNode;
 
     return tail;
+}
+void deleteBeg(){
+    if (tail==NULL)
+    {
+        printf("\nAlready Empty");
+        return;
+    }
+    if(tail->next==tail){
+       free(tail);
+       tail = NULL;
+       return;
+    }
+    struct Node * temp = tail->next;
+    tail->next = tail->next->next;
+    free(temp);
+}
+void deleteEnd(){
+    if(tail==NULL){
+        printf("\nAlready empty.");
+        return;
+    }
+    if(tail->next==tail){
+       free(tail);
+       tail = NULL;
+       return;
+    }
+    struct Node * temp = tail->next;
+
+    while(temp->next!=tail){
+        temp=temp->next;
+    }
+    
+    temp->next=tail->next;
+    free(tail);
+    tail = temp;
 }
 void display(){
     if (tail==NULL)
@@ -89,7 +128,16 @@ int main(){
         insertBeg(10);
         insertBeg(60);
         tail = insertEnd(41); 
-        tail = insertAtPos(4,20);
+        tail = insertAtPos(3,20);
+        deleteBeg();
+        deleteBeg();
+        // deleteBeg();
+        // deleteBeg();
+        // deleteBeg();
+        // deleteBeg();
+        deleteEnd();
+        deleteEnd();
+        deleteEnd();
         display();
 
 }
